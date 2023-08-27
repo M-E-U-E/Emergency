3a44387e8b72be732c1b1774311a6fbf8b345b6c
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2023-08-28 05:14:54 +0600

    Add files via upload

---
+++ B. Different Divisors.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+bool isPrime(int n)
+{
+    // Corner cases
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+
+    // This is checked so that we can skip
+    // middle five numbers in below loop
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+
+    return true;
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll ans1=0, ans2=0;
+    ll x = 1+n;
+    while(true)
+    {
+        if(isPrime(x))
+        {
+            ans1 = x;
+            break;
+        }
+        x++;
+    }
+    x += n;
+    while(true)
+    {
+        if(isPrime(x))
+        {
+            ans2 = x;
+            break;
+        }
+        x++;
+    }
+    cout << ans1*ans2 << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
+
+
+
+
---
+++ C. Celex Update.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+void solve()
+{
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+    ll x = (c-a)*(d-b);
+    cout << x+1 << endl;
+
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
+
+
+
